64c99fd27fd8271d4f39d2e28872b13f6f3562b6
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-04 22:36:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+    const [load, setLoad] = useState(false);
+    const [url, setUrl] = useState('');
+    const [title, setTitle] = useState('');
+    const [flag, setFlag] = useState(false);
+    const handleChagne = async(e) => {
+        let id = e.target.value;
+        if (id >=1 && id <= 5000) {
+        setLoad(true);
+        setFlag(false)
+      
+        const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
+
+        const data = await response.json();
+        setLoad(false);
+        setTitle(data.title);
+        setUrl(data.url)
+        setFlag(true)
+       
+        console.log(data);
+        }
+
+
+
+    }
+
+    return (
+        <>
+            <label>Id number</label>
+            <input type='number' onChange={handleChagne}/>
+            {load && <Loader/>}
+            {flag && <PhotoFrame url={url} title={title}/>}
+        </>
+    )
 }
 
 
