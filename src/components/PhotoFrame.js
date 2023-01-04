79e7c76d3496931586e55edfbe2c46553fdabc3b
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-04 22:38:18 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = (propes) => {
+   return(
+    <div className='photoframe'>
+        <img src={propes.url}></img>
+        <div className='caption'>
+            {propes.title}
+        </div>
+    </div>
+   )
 }
